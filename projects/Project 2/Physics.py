4e104be296cf59c692e703ad2a5fc41d30d13c18
Perseus52 <kai.s.hillery@gmail.com>
2026-03-23 16:50:31 -0400

    code is completely finished project is done

--- projects/Project 2/Physics.py
+++ projects/Project 2/Physics.py
@@ -6,5 +6,13 @@
 import math
 
 def drag_force(velocity: float, fluid_density: float, drag_coefficient: float, diameter: float) -> float:
-    area = 3.14159 * (diameter / 2) ** 2
-    return 0.5 * fluid_density * drag_coefficient * area * velocity * abs(velocity)
+    return 0.5 * fluid_density * drag_coefficient * (math.pi/4) * diameter**2 * velocity * abs(velocity)
+
+def buoyant_force(fluid_density: float, diameter: float) -> float:
+    return (math.pi/6) * fluid_density * diameter**3
+
+def grav_force(fluid_density: float, diameter: float) -> float:
+    return (math.pi/6) * fluid_density * diameter**3
+
+def e_field_force(cloud_density: float, distance: float, height: float) -> float:
+    return (cloud_density/(2*8.854187817e-12)) * ((2 * distance) - height)
